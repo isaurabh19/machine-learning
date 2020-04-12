3dadd4ae0343a1020f65e659efe0da071cc7de39
viru <virender13117@gmail.com>
2020-04-12 16:14:08 -0400

    logistic regression model updated

--- source/models/logistic_regression.py
+++ source/models/logistic_regression.py
@@ -9,12 +9,14 @@ from sklearn.model_selection import KFold, StratifiedKFold
 
 class Logistic_Regression_model:
     def train(self, X):
+        accuracy_scores = []
+        roc_scores = []
         kf = StratifiedKFold(n_splits=10, shuffle=False)
         for train_index, test_index in kf.split(X[:, :-1], X[:, -1]):
             X_train, X_test = X[train_index, :], X[test_index, :]
             Y_train, Y_test = X_train[:, -1], X_test[:, -1]
             clf = LogisticRegression(random_state=0).fit(X_train, Y_train)
-            Y_pred = clf.predict(Y_test)
+            Y_pred = clf.predict(X_test)
             roc_scores[i].append(roc_auc_score(Y_test, Y_pred))
             accuracy_scores[i].append(accuracy_score(Y_test, Y_pred))
         return np.mean(accuracy_scores), np.mean(roc_scores)
